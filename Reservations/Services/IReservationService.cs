583b8ccc5d93365c32f6fa63d68dfbebee91a78c
marvin.hinkley <marvin.hinkley@americaneagle.com>
2024-08-06 10:02:38 -0700

    in-memory data store

--- Reservations/Services/IReservationService.cs
+++ Reservations/Services/IReservationService.cs
@@ -4,6 +4,8 @@ namespace Reservations.Services;
 
 public interface IReservationService
 {
+    private static List<Reservation> _reservations = [];
+    
     // Retrieves list of available reservations
     Task<IEnumerable<Reservation>> GetAvailableReservations(
         TimeRange? timeRange = null,
@@ -11,17 +13,20 @@ public interface IReservationService
         CancellationToken cancellationToken = default
     );
 
+    // Updates available reservations for a provider
     Task UpdateReservations(
         IEnumerable<Reservation> reservations,
         Guid providerId,
         CancellationToken cancellationToken = default
     );
 
+    // Schedule a reservation for a particular time
     Task ScheduleReservation(
         Reservation reservation,
         CancellationToken cancellationToken = default
     );
     
+    // Confirm a scheduled resrevation
     Task ConfirmReservation(
         Guid id,
         CancellationToken cancellationToken = default
